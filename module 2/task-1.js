9d067ba2c27571ce0a69b3b311dc80c9f3c7b9f0
Koval <dmytrokovalskyi@yahoo.com>
2020-07-21 01:11:35 +0300

    4 первых задания

---
+++ module 2/task-1.js
@@ -0,0 +1,9 @@
+const logItems = function(array) {
+    let count=1;
+    for (let i=0; i< array.length; i++ )
+    
+{console.log (`${count} ":" ${array[i]}`)
+count +=1;
+    ;}}
+
+    logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50])
---
+++ module 2/task-3.js
@@ -0,0 +1,22 @@
+const findLongestWord = function(string) {
+    
+        let arrStr = string.split(' ');
+        let wordLength = 0;
+        let longestWord;
+      
+        for (let i = 0; i < arrStr.length; i += 1) {
+          wordLength = arrStr[1].length;
+      
+          if (arrStr[i].length > wordLength) {
+            longestWord = arrStr[i];
+      
+            return longestWord;
+          }
+      
+        }
+      };
+      console.log(findLongestWord('The quick brown fox jumped over the lazy dog')); // 'jumped'
+
+      console.log(findLongestWord('Google do a roll')); // 'Google'
+      
+      console.log(findLongestWord('May the force be with you')); // 'force'
