34b2723bb67f5da02614df211313b4a6d51a03d7
Koval <dmytrokovalskyi@yahoo.com>
2020-07-21 16:25:46 +0300

    практика

---
+++ module 2/task-5.js
@@ -0,0 +1,14 @@
+// const ask = function (question , yes, no) 
+//    { (confirm(question)) ? yes() :  no()}
+
+
+//    ask(
+//     "Вы согласны?",
+//     () => alert("Вы согласились."),
+//     () => alert("Вы отменили выполнение.")
+//   );
+  const string =  function (str = "", n=2) {
+
+    console.log(str.repeat(n));
+  }
+string("fdsvs", );
