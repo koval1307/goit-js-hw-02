50f510a48d344d76bed38d9db732b7f05099f5a6
Koval <dmytrokovalskyi@yahoo.com>
2020-07-28 13:12:41 +0300

    5я домашка ( 4 задания)

---
+++ module 4/practice.js
@@ -0,0 +1,31 @@
+// ("use strict")
+// const storeStatiscs = {
+//     shirt : 0,
+//     pants : 0,
+//     shoes : 0,
+//  };
+
+
+// const SoldItems = function (name, size ,brand, amount) {
+//     this.name = name;
+//     this.size = size;
+//     this.brand = brand;
+//     this.amount = amount;
+//     this. greet  = function (customer){
+//         console.log (`Hello ${customer}, you purchased ${amount} ${this.brand} ${this.name} in size ${size}`);
+//     }
+// this.itemSold = function(amount, name) {
+// if (name === "shirt") {storeStatiscs.shirt += amount}
+
+// else if (name === "pants") {storeStatiscs.pants += amount}
+
+
+// else if (name === "shoes") {storeStatiscs.shoes += amount}
+// }}
+// const soldItems = new SoldItems("shirt" , "l" , "adidas", 2);
+
+// soldItems.greet ("Ivan");
+// soldItems.itemSold(4,"shirt");
+// soldItems.itemSold(2,"shoes");
+
+// console.log (storeStatiscs);
