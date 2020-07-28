dcac6f5b1ff0f6ad22c2572ba239e1fd75185cd1
Koval <dmytrokovalskyi@yahoo.com>
2020-07-28 23:18:27 +0300

    модуль 5 задание 5

--- module 5/practice.js
+++ module 5/practice.js
@@ -29,3 +29,40 @@
 // soldItems.itemSold(2,"shoes");
 
 // console.log (storeStatiscs);
+
+//1
+// const a = ['a', 'b', 'c'];
+// const b = [1,2,3];
+// let c = [...a, ...b];
+// console.log (c);
+//2
+// b.push(a[0], a[1], a[2]);
+// console.log (b);
+//3
+// const b = [1,2,3];
+// const reversed = b.reverse();
+// console.log(reversed);
+//4
+// const d = [1,2,3,4,5];
+// console.log (d.slice(0,3))
+
+//5    
+
+// let t = [1, 2, 3, 4, 5];
+// t.splice(1,2)
+// console.log (t);
+
+
+
+// const a = [1, 2, 3, 4, 5]; 
+
+
+// b=Math.floor(Math.random() * a.length)
+// console.log (a[b]);
+
+
+// let b = prompt ('введите число');
+// b = b.split("");
+// let arr = b;
+
+// const basicMoves = ["камень", "ножницы", "бумага"];
